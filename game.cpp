5bb0c2aa261ed258f7739a82925220f079cfea45
Pavel Dolgov <pdolgov99@gmail.com>
2014-09-09 20:24:38 +0400

    added type cheking to game_for_win, game_with_time

--- game.cpp
+++ game.cpp
@@ -1,17 +1,29 @@
 #include <time.h>
 #include <stdlib.h>
 #include <iostream>
+#include <limits>
 
 #include "game_desk.hpp"
 
 void game_for_win() {
     int k, h;
-    std::cout << "What number you want to finish the game? ";
-    std::cin >> k;
+    k = 0;
     std::cout << "Choose the size of game desk (not very big, "
             "depends on the size of your screen): ";
-    std::cin >> h;
+    if (std::cin >> h) {
+    }
+    else {
+        std::cout << "Error. It is not a number!" << std::endl;
+        return;
+    }
     GameDesk desk(h);
+    std::cout << "What number you want to finish the game? ";
+    if (std::cin >> k) {
+    }
+    else {
+        std::cout << "Error. It is not a number!" << std::endl;
+        return;
+    }
     desk.output();
     while (!desk.check_fail() && !desk.check_win(k)) {
         desk.play();
@@ -35,17 +47,27 @@ void game_for_score() {
 void game_with_time() {
     int k, h;
     std::cout << "How many time you want to play (min)? ";
-    std::cin >> k;
+    if (std::cin >> k) {
+    }
+    else {
+        std::cout << "Error. It is not a number, dummy!" << std::endl;
+        return;
+    }
     std::cout << "Choose the size of game desk (not very big, "
             "depends on the size of your screen): ";
-    std::cin >> h;
+    if (std::cin >> h) {
+    }
+    else {
+        std::cout << "Error. It is not a number, dummy!" << std::endl;
+        return;
+    }
     GameDesk desk(h);
     desk.output();
     int t = time(NULL);
     int t1 = 0;
     while (!desk.check_fail() && (t1-t) <= k * 60) {
         desk.play();
-        int t1 = time(NULL);
+        t1 = time(NULL);
     }
     desk.finish();      
 }
